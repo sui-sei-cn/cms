5f35e855e843d2c3e07c91d536c7d217a84979d5
suisei <2456014078@qq.com>
2024-12-31 13:19:26 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  app
+
+  <el-button type="primary">Primary</el-button>
+  <el-button type="success">Success</el-button>
+</template>
+
+<style scoped></style>
